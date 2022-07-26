d66c43ba9f51de1f998054266c200c881e2f5e6e
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-26 09:14:32 +0500

    Add header and item card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Header from './app/components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <>
-    <div>waleed</div>
+      <Header ></Header>
     </>
   );
 }
