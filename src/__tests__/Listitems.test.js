8ece1d739e6477daa93a001ec49a71a97db9a5ff
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-26 11:36:11 +0500

    Add tests for Listitem

---
+++ src/__tests__/Listitems.test.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import Listitems from '../app/components/Listitems/Listitems';
+import store from '../redux/configureStore';
+
+it('renders properly', () => {
+  const tree = renderer.create(
+    <Router>
+      <Provider store={store}>
+        <Listitems />
+      </Provider>
+    </Router>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
