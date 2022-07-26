41311f1485320b2ac20f44ee2cbffbac0cb044f6
Waleed Amjad <waleedamjad99@hotmail.com>
2022-07-26 11:34:09 +0500

    Add tests for header and itemCard

---
+++ src/__tests__/ItemCard.test.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import ItemCard from '../app/components/ItemCard/ItemCard';
+import store from '../redux/configureStore';
+
+it('renders properly', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <Provider store={store}>
+          <ItemCard />
+        </Provider>
+      </Router>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/__tests__/header.test.js
@@ -0,0 +1,13 @@
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../app/components/Header/Header';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Router>
+      <Header />
+    </Router>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
