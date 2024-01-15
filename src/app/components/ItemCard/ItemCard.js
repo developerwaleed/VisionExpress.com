cd483cb1daae63bc48e5296f1ff1176b40f9bf9b
Waleed Amjad <waleedamjad99@hotmail.com>
2024-01-16 00:01:01 +0500

    Improved Item Card

--- src/app/components/ItemCard/ItemCard.js
+++ src/app/components/ItemCard/ItemCard.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import styles from './ItemCard.module.css';
 
-const ProductComponent = () => {
-  const products = useSelector((state) => state.allProducts.products);
-  if (products.length === 0) {
+const ProductComponent = ({ displayedProducts }) => {
+
+  if (displayedProducts.length === 0) {
     return (
       <div className={styles.loadingHome}>
-        <div className={styles.Loader} />
+        <img src="/loading.webp" width="50px" alt="Loading" />
       </div>
     );
   }
-  const renderList = products.map((product) => {
-    const {
-      id, title, image, price, category,
-    } = product;
-    return (
-      <div className={styles.item} key={id}>
-        <Link to={`/product/${id}`}>
-          <div className={styles.itemCard}>
-            <div className={styles.itemImage}>
-              <img src={image} alt={title} />
+
+  const renderList = 
+  displayedProducts.map((product) => {
+      const {
+        id, title, image, price, category,
+      } = product;
+
+      return (
+        <div className={styles.item} key={id}>
+          <Link to={`/product/${id}`}>
+            <div className={styles.itemCard}>
+              <div className={styles.itemImage}>
+                <img src={image} alt={title} width="50px" />
+              </div>
+              <div className={styles.itemDescription}>
+                <h3 className={styles.itemName}>{title}</h3>
+                <h4 className={styles.itemPrice}>
+                  $
+                  {price}
+                </h4>
+                <h5 className={styles.itemCategory}>{category}</h5>
+              </div>
             </div>
-            <div className={styles.itemDescription}>
-              <h3 className={styles.itemName}>{title}</h3>
-              <h4 className={styles.itemPrice}>
-                $
-                {price}
-              </h4>
-              <h5 className={styles.itemCategory}>{category}</h5>
-            </div>
-          </div>
-        </Link>
-      </div>
-    );
-  });
+          </Link>
+        </div>
+      );
+    })
+
   return <>{renderList}</>;
 };
 
